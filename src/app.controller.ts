4d5be31bae6ff3631a9c021a0a218c6f214020eb
Jonathan Abreu <jonathanvabreu26@gmail.com>
2025-07-11 20:29:06 -0300

    health

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Head, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
@@ -8,5 +9,10 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Head('/health')
+  healthCheck(@Res() res: Response) {
+    res.status(200).end();
   }
 }
